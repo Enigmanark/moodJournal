237f1af14e752a24e8f7295e2bbafd6d0e2a4c7d
Keleiron <johnathan.crocker@gmail.com>
2017-09-10 18:10:41 -0500

    Updated title for nav controller

--- src/main/java/journal/navController.java
+++ src/main/java/journal/navController.java
@@ -1,15 +1,35 @@
 package journal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class navController {
+public class NavController {
+    private String title = Application.TITLE + " " + Application.VERSION;
 
-    @GetMapping("/")
-    public String home() {
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", title);
         return "home";
     }
 
+    @GetMapping("/user/journal")
+    public String journal(Model model) {
+        model.addAttribute("title", "Journal - " + title);
+        return "journal";
+    }
 
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Contact - " + title);
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - " + title);
+        return "about";
+    }
 }
