9522d126d50a2ebf0082f98ae55bbdd5d9e5197d
Keleiron <johnathan.crocker@gmail.com>
2017-09-11 13:43:08 -0500

    Added some styling to entries

--- src/main/java/journal/Application.java
+++ src/main/java/journal/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
     public final static String TITLE = "Mood Journal";
-    public final static String VERSION = "0.5.0 Alpha";
+    public final static String VERSION = "0.6.2 Alpha";
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(Application.class, args);
 
